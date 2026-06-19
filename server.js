2bb80485cbebc189d882f893d8a2fe674395c747
@abolfazl.tabe95 <abolfazl.tabe95@gmail.com>
2026-06-19 18:28:06 +0330

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello Matin Teb"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
